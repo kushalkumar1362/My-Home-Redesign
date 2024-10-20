02a2d73551f1bbd40c17305253a6ed0c994d9bf4
kushalkumar1362 <kushalkumar1362@gmail.com>
2024-10-20 12:45:58 +0530

    intial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter } from 'react-router-dom'; // for router and route
+import { Toaster } from 'react-hot-toast'; // to show the toast on success fail loading etc...
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
